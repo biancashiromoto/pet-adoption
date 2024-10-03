d7caf39fd780ff45d28f01063181d53b032c6cd2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-03 19:34:57 -0300

    build: install catName dependencies and add names to pets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
-import './App.css'
+import './App.css';
+import { catNames } from 'cat-names';
 
 const MAX_PETS = 15;
 const API_KEY: string = 'live_zllOBSsaSmrsLy6r2n5z2SQ7Zqz4NkckgTWwPzxZJr90rcoeMUpSleldcvpv8v9r';
 const API_URL: string = `https://api.thecatapi.com/v1/images/search?limit=${MAX_PETS}&size=thumb `;
 
 interface Pet {
+  age?: number;
   id: string;
   height: number;
+  name?: string;
   url: string;
   width: number;
 }
@@ -27,6 +30,12 @@ const fetchPetsFromAPI = async (url: string) => {
   }});
   const data = await response.json();
   const filteredData = data.filter((pet: Pet) => pet.url.split('.').pop() !== 'gif');
+  filteredData.forEach((pet: Pet) => {
+    const randomIndex = Math.round((Math.random() * 100));
+    const name = catNames[randomIndex];
+    pet.name = name;    
+  });
+  
   return filteredData;
 }
 
@@ -47,9 +56,17 @@ function App() {
     } 
   }, []);
 
+  useEffect(() => console.log(pets), [pets])
+
   return (
     <main>
       <h1>Pet adoption</h1>
+      {pets && pets.map(pet => (
+        <div key={pet.id}>
+          <img alt="Picture of a cat" src={pet.url} />
+          <h3>{pet.name}</h3>
+        </div>
+      ))}
     </main>
   )
 }
