bc53bc0053afd9515d5eadec61567e9feacd3638
Bianca Shiromoto <p22.bianca@webmotors.com.br>
2024-10-04 11:51:24 -0300

    feat: create Modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, SetStateAction } from "react";
+import { Pet } from "../../App"
+
+interface ModalProps {
+    pet: Pet[];
+    setShowModal: Dispatch<SetStateAction<boolean>>;
+}
+
+const Modal = ({ pet, setShowModal }: ModalProps) => {
+  return (
+    <div className='modal'>
+        <h2>{`Are you sure you want to adopt ${pet[0].name}?`}</h2>
+        <div className="modal__buttons-container">
+            <button
+                aria-label="Yes"
+                type="button"
+                onClick={() => console.log('Yes')}
+            >
+                Yes
+            </button>
+            <button
+                aria-label="No"
+                type="button"
+                onClick={() => setShowModal(false)}
+            >
+                No
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Modal
