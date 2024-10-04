888c9c6d50864fd5e1a143a8c37911073ce1a404
Bianca Shiromoto <p22.bianca@webmotors.com.br>
2024-10-04 14:58:39 -0300

    feat: implement route to /adopt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import AdoptionForm from './pages/AdoptionForm';
 
 function App() {
   return (
@@ -10,9 +11,11 @@ function App() {
         <strong>Attention: </strong>
         This is a fake adoption site created to practice my development skills. None of these pets can be adopted through this page.
       </p>
+      <hr />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/adopt" element={<AdoptionForm />} />
         </Routes>
       </Router>
     </>
