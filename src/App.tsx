3c0db24f2d58ec68e76928424cc4a760bd5bb1dc
Bianca Shiromoto <p22.bianca@webmotors.com.br>
2024-10-04 14:35:18 -0300

    feat: add explanatory text and implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <h1>Pet adoption</h1>
+      <p>
+        <strong>Attention: </strong>
+        This is a fake adoption site created to practice my development skills. None of these pets can be adopted through this page.
+      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
