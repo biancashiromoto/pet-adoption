0cf35e28574eef033754be21fb557934b81b66e2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-05 21:08:20 -0300

    feat: create Loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,15 @@
+import Loading from '../../assets/loading.gif'
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+      Loading...
+      <img
+        alt='Loading gif'
+        src={Loading}
+      />
+    </div>
+  )
+}
+
+export default Loader
