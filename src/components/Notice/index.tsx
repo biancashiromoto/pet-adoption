49be0e5f66e4dde51da605db2869d07114e5b4ce
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-05 23:09:33 -0300

    feat: implement close button in Notice component

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -1,12 +1,21 @@
-import { ReactNode } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import { Button } from "../Button";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 interface NoticeProps {
   children: ReactNode;
+  setShowNotice: Dispatch<SetStateAction<boolean>>;
 }
 
-const Notice = ({ children }: NoticeProps) => {
+const Notice = ({ children, setShowNotice }: NoticeProps) => {
   return (
     <section className='notice'>
+      <Button.Root
+        onClick={() => setShowNotice(false)}
+        ariaLabel="Close notice"
+      >
+        <IoMdCloseCircleOutline />
+      </Button.Root>
       { children }
     </section>
   )
