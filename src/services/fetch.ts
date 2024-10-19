7115b5fca999359dd52f732b8d4bd2e4216888e9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-19 20:00:36 -0300

    refactor: create function fetchPets

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,6 +1,7 @@
 import { Utils } from "./Utils";
-import { Pet } from "../types/Pet";
+import { PetData } from "../types/PetData";
 import { info } from "./info";
+import { Pets } from "../context/Provider";
 
 const utils = new Utils();
 
@@ -8,7 +9,7 @@ export const fetchDogs = async () => {
   const response = await fetch(info.DOG_API_URL, {
     headers: { 'x-api-key': info.DOG_API_KEY },
   });
-  const data: Pet[] = await response.json();
+  const data: PetData[] = await response.json();
   const filtered = utils.removeGifs(data);
   return utils.addPetsInfo(filtered);
 };
@@ -17,7 +18,13 @@ export const fetchCats = async () => {
   const response = await fetch(info.CAT_API_URL, {
     headers: { 'x-api-key': info.CAT_API_KEY },
   });
-  const data: Pet[] = await response.json();
+  const data: PetData[] = await response.json();
   const filtered = utils.removeGifs(data);
   return utils.addPetsInfo(filtered);
+}
+
+export const fetchPets = async (): Promise<Pets> => {
+  const cats = await fetchCats();
+  const dogs = await fetchDogs();
+  return { cats, dogs };
 }
