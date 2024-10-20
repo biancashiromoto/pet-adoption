e4723184ddb6db48b17744eca0e0eef077b39475
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-19 22:53:32 -0300

    tests: implement unit tests for component Loader

---
+++ src/components/Loader/spec/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Loader from "..";
+
+describe('Loader component', () => {
+  it('should be correctly rendered', () => {
+    render(<Loader />);
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('src', '/src/assets/loading.gif')
+  });
+})
