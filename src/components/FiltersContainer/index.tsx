1f969c1e4790edf86ecd141267ea898788368797
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-19 22:45:42 -0300

    feat: implement component SpeciesFilter

--- src/components/FiltersContainer/index.tsx
+++ src/components/FiltersContainer/index.tsx
@@ -1,35 +1,9 @@
-import { useContext } from 'react';
-import { Context } from '../../context';
+import SpeciesFilter from '../SpeciesFilter';
 
 const FiltersContainer = () => {
-  const {
-    species,
-    setSpecies
-  } = useContext(Context);
   return (
     <article className='filter-container'>
-      <section className='filters__species'>
-        <label htmlFor='cats'>
-          <input
-            checked={species === 'cats'}
-            id="cats"
-            type='radio'
-            name='species'
-            onChange={() => setSpecies('cats')}
-          />
-          Cats
-        </label>
-        <label htmlFor='dogs'>
-          <input
-            checked={species === 'dogs'}
-            id="dogs"
-            type='radio'
-            name='species'
-            onChange={() => setSpecies('dogs')}
-          />
-          Dogs
-        </label>
-      </section>
+      <SpeciesFilter />
     </article>
   )
 }
