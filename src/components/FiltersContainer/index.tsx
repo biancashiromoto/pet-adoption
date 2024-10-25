3a823279fcd5badf63a80ba1d32ee1c4c087b0e8
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-25 18:56:02 -0300

    feat: implement order by age filter

--- src/components/FiltersContainer/index.tsx
+++ src/components/FiltersContainer/index.tsx
@@ -1,9 +1,21 @@
+import { useContext } from 'react';
+import Filter from '../Filter';
 import SpeciesFilter from '../SpeciesFilter';
+import { FiltersContainerProps, OrderByAgeFilter } from './index.types';
+import { Context } from '../../context';
 
-const FiltersContainer = () => {
+const FiltersContainer = ({speciesRef}: FiltersContainerProps) => {
+  const { setOrder } = useContext(Context);
   return (
     <article className='filter-container'>
       <SpeciesFilter />
+      <Filter
+        id='order-by-age'
+        label='Order by:'
+        items={['none', 'younger', 'older']}
+        onChange={() => setOrder(speciesRef?.current?.value as OrderByAgeFilter)}
+        ref={speciesRef}
+      />
     </article>
   )
 }
