976c368cac5e046f24a41b2d8af6ad0ad21e6581
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-26 17:54:43 -0300

    feat: implement filter by favorites types

--- src/context/index.types.ts
+++ src/context/index.types.ts
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { PetData } from "../types/PetData";
 import {
+  FavoritesFilter,
   OrderByAgeFilter,
   SpeciesFilter,
 } from "../components/FiltersContainer/index.types";
@@ -21,4 +22,6 @@ export interface ContextProps {
   setSpecies: Dispatch<SetStateAction<SpeciesFilter>>;
   order: OrderByAgeFilter;
   setOrder: Dispatch<SetStateAction<OrderByAgeFilter>>;
+  favorites: FavoritesFilter;
+  setFavorites: Dispatch<SetStateAction<FavoritesFilter>>;
 }
