9768122e638433e068b803f1a3b583bcfc513290
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-26 11:19:15 -0300

    tests: add data-testid to select element

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -4,7 +4,7 @@ import { FilterProps } from './index.types';
 const Filter = forwardRef<HTMLSelectElement, FilterProps>(({ id, label, items, onChange }, ref) => (
   <div className='filter-container__filter'>
     <label htmlFor={id}>{label}</label>
-    <select id={id} ref={ref} onChange={onChange}>
+    <select id={id} ref={ref} onChange={onChange} data-testid="filter-select">
       {items.map((item) => (
         <option key={item} value={item}>
           {item}
