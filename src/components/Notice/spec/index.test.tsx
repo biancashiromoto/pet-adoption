bdd6d56cf5bf12a2440572aac15f1d93a838fbb4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-27 17:25:59 -0300

    tests: implement unit tests for Notice component

---
+++ src/components/Notice/spec/index.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import Notice from "..";
+
+describe("Modal component", () => {
+  const setShowNotice: vi.Mock = vi.fn();
+
+  beforeEach(() => {
+    render(
+      <Notice setShowNotice={setShowNotice}>
+        <h2>Notice</h2>
+      </Notice>
+    );
+  });
+
+  it("should be correctly rendered", () => {
+    expect(
+      screen.getByRole("heading", { name: /notice/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should call onClick function when button is clicked", async () => {
+    fireEvent.click(screen.getByRole("button"));
+    expect(setShowNotice).toHaveBeenCalledWith(false);
+  });
+});
