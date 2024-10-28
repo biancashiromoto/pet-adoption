6d514f78671f502483ace9a576a46994fa67a4e7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-27 21:42:57 -0300

    refactor: implement tanstack-query to manage API requests

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,15 @@
-import { createRef, useContext, useEffect } from 'react';
-import Modal from '../../components/Modal';
-import { Button } from '../../components/Button';
-import { useNavigate } from 'react-router-dom';
-import Overlay from '../../components/Overlay';
-import { Utils } from '../../helpers/Utils';
-import { info } from '../../helpers/info';
-import Card from '../../components/Card';
-import FiltersContainer from '../../components/FiltersContainer';
-import Loader from '../../components/Loader';
-import { Pet } from '../../types/Pet';
-import { Context } from '../../context';
-
-const utils = new Utils();
+import { createRef, useContext, useEffect } from "react";
+import Modal from "../../components/Modal";
+import { Button } from "../../components/Button";
+import { useNavigate } from "react-router-dom";
+import Overlay from "../../components/Overlay";
+import Card from "../../components/Card";
+import FiltersContainer from "../../components/FiltersContainer";
+import Loader from "../../components/Loader";
+import { Pet } from "../../types/Pet";
+import { Context } from "../../context";
+import fetchPets from "../../services/fetchPets";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
   const speciesRef = createRef<HTMLSelectElement>();
@@ -28,102 +26,63 @@ const Home = () => {
     setShowAdoptionModal,
     showUpdatePetsModal,
     setShowUpdatePetsModal,
-    isLoading,
-    setIsLoading,
     speciesFilter,
     setSpeciesFilter,
     orderFilter,
     setOrderFilter,
     favoritesFilter,
     setFavoritesFilter,
-    error,
-    setError
   } = useContext(Context);
   const navigate = useNavigate();
+
+  const {
+    isLoading,
+    error,
+    data: fetchedPets,
+  } = useQuery({
+    queryKey: ["fetchPets"],
+    queryFn: fetchPets,
+    staleTime: Infinity,
+  });
+
+  useEffect(() => {
+    setPets(fetchedPets || []);
+  }, [fetchedPets]);
 
   const resetFavorites = () => {
     const updatedPets = pets.map((pet: Pet) => ({
       ...pet,
-      isFavorite: false
+      isFavorite: false,
     }));
     setPets(updatedPets);
     setDisplayedPets(updatedPets);
-    utils.setLocalStorage('pets', updatedPets);
   };
-
-  const fetchPets = async () => {
-    setIsLoading(true);
-    try {
-      const catsResponse = await fetch(info.CAT_API_URL, {
-        headers: { 'x-api-key': info.CAT_API_KEY }
-      });
-      const dogsResponse = await fetch(info.DOG_API_URL, {
-        headers: { 'x-api-key': info.DOG_API_KEY }
-      });
-      
-      if (!catsResponse.ok || !dogsResponse.ok) {
-        throw new Error('Failed to fetch');
-      }
-      
-      const catsData = await catsResponse.json();
-      const dogsData = await dogsResponse.json();
-      
-      const filteredCatsData = utils.removeGifs(catsData);
-      const filteredDogsData = utils.removeGifs(dogsData)
-      const allPets = filteredCatsData.concat(filteredDogsData);
-      utils.addPetsInfo(allPets);
-      utils.shuffleArray(allPets);
-
-      setPets(allPets);
-      setDisplayedPets(allPets);
-      utils.setLocalStorage('pets', allPets);
-    } catch (error) {
-      if (error instanceof Error) {
-        setError(error.message);
-      } else {
-        setError('An unknown error occurred');
-      }
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }
 
   const applyFilters = () => {
     let filteredPets = pets;
 
-    if (speciesFilter !== 'all') {
+    if (speciesFilter !== "all") {
       filteredPets = pets.filter((pet: Pet) => pet.species === speciesFilter);
     }
 
-    if (favoritesFilter !== 'all') {
-      if (favoritesFilter === 'favorites') {
+    if (favoritesFilter !== "all") {
+      if (favoritesFilter === "favorites") {
         filteredPets = filteredPets.filter((pet: Pet) => pet.isFavorite);
       } else {
         filteredPets = filteredPets.filter((pet: Pet) => !pet.isFavorite);
       }
     }
-    
+
     let orderedPets = filteredPets;
-    if (orderFilter !== 'none') {
+    if (orderFilter !== "none") {
       orderedPets = [...filteredPets].sort((a: Pet, b: Pet) => {
         if (!a.age || !b.age) return 0;
-        return orderFilter === 'younger' ? a.age - b.age : b.age - a.age;
+        return orderFilter === "younger" ? a.age - b.age : b.age - a.age;
       });
     }
     setDisplayedPets(orderedPets);
   };
 
-  useEffect(() => {
-    const storaged = utils.getLocalStorage('pets');
-    if (!storaged || storaged.length === 0) {
-      fetchPets();
-    } else {
-      setPets(storaged);
-      setDisplayedPets(storaged);
-    }
-  }, []);
-  
   useEffect(() => {
     setShowUpdatePetsModal(false);
     setShowAdoptionModal(false);
@@ -135,7 +94,7 @@ const Home = () => {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
+      if (event.key === "Escape") {
         if (showAdoptionModal) {
           setShowAdoptionModal(false);
         }
@@ -145,10 +104,10 @@ const Home = () => {
       }
     };
 
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener("keydown", handleKeyDown);
     };
   }, [showAdoptionModal, showUpdatePetsModal]);
 
@@ -164,46 +123,44 @@ const Home = () => {
 
   const clearFilters = () => {
     setDisplayedPets(pets);
-    setOrderFilter('none');
-    setSpeciesFilter('all');
-    setFavoritesFilter('all');
+    setOrderFilter("none");
+    setSpeciesFilter("all");
+    setFavoritesFilter("all");
 
     if (speciesRef.current) {
-      speciesRef.current.value = 'all';
+      speciesRef.current.value = "all";
     }
-    
+
     if (orderRef.current) {
-      orderRef.current.value = 'none';
+      orderRef.current.value = "none";
     }
 
     if (favoriteRef.current) {
-      favoriteRef.current.value = 'all';
+      favoriteRef.current.value = "all";
     }
-  }
+  };
 
-  const toggleFavorite = (id: Pet['id']) => {
+  const toggleFavorite = (id: Pet["id"]) => {
     setPets((prevPets: Pet[]) => {
-      const updatedPets = prevPets.map(pet => {
+      const updatedPets = prevPets.map((pet) => {
         if (pet.id === id) {
           return { ...pet, isFavorite: !pet.isFavorite };
         }
         return pet;
       });
-      
-      utils.setLocalStorage('pets', updatedPets);
-      
+
       return updatedPets;
     });
-  }
+  };
 
   return (
     <>
-      {error && <p>Error: {error}</p>}
+      {error && <p>Error: {error.message}</p>}
       <Button.Root
-        ariaLabel='Update pets'
+        ariaLabel="Update pets"
         onClick={() => setShowUpdatePetsModal(true)}
       >
-        <Button.Label label='Update pets' />
+        <Button.Label label="Update pets" />
       </Button.Root>
       <hr />
       <FiltersContainer
@@ -218,36 +175,40 @@ const Home = () => {
       {!error && !isLoading && (
         <>
           <main>
-            {displayedPets && displayedPets.map((pet: Pet) => (
-              <Card
-                key={pet.id}
-                pet={pet}
-                setSelectedPet={setSelectedPet}
-                setShowModal={setShowAdoptionModal}
-                toggleFavorite={toggleFavorite}
-              />
-            ))}
+            {displayedPets &&
+              displayedPets.map((pet: Pet) => (
+                <Card
+                  key={pet.id}
+                  pet={pet}
+                  setSelectedPet={setSelectedPet}
+                  setShowModal={setShowAdoptionModal}
+                  toggleFavorite={toggleFavorite}
+                />
+              ))}
           </main>
           {showAdoptionModal && (
             <>
               <Overlay openModal={setShowAdoptionModal} />
               <Modal
                 title={`Would you like to adopt ${selectedPet[0].name}?`}
-                text='You will be redirected to the adoption form'
+                text="You will be redirected to the adoption form"
               >
-                <img alt={`Random picture of a ${selectedPet[0].species}`} src={selectedPet[0].url} />
-                <div className='modal__buttons-container'>
+                <img
+                  alt={`Random picture of a ${selectedPet[0].species}`}
+                  src={selectedPet[0].url}
+                />
+                <div className="modal__buttons-container">
                   <Button.Root
-                    ariaLabel='Yes'
-                    onClick={() => navigate('/adopt')}
+                    ariaLabel="Yes"
+                    onClick={() => navigate("/adopt")}
                   >
-                    <Button.Label label='Yes' />
+                    <Button.Label label="Yes" />
                   </Button.Root>
                   <Button.Root
-                    ariaLabel='No'
+                    ariaLabel="No"
                     onClick={() => setShowAdoptionModal(false)}
                   >
-                    <Button.Label label='No' />
+                    <Button.Label label="No" />
                   </Button.Root>
                 </div>
               </Modal>
@@ -255,26 +216,26 @@ const Home = () => {
           )}
           {showUpdatePetsModal && (
             <>
-              <Overlay openModal={setShowUpdatePetsModal}/>
+              <Overlay openModal={setShowUpdatePetsModal} />
               <Modal
-                text='Updating pets will remove the current list and fetch new ones. Do you want to continue?'
-                title='Update pets?'
+                text="Updating pets will remove the current list and fetch new ones. Do you want to continue?"
+                title="Update pets?"
               >
-                <div className='modal__buttons-container'>
+                <div className="modal__buttons-container">
                   <Button.Root
-                    ariaLabel='Yes'
+                    ariaLabel="Yes"
                     onClick={() => {
                       fetchPets();
                       setShowUpdatePetsModal(false);
                     }}
                   >
-                    <Button.Label label='Yes' />
+                    <Button.Label label="Yes" />
                   </Button.Root>
                   <Button.Root
-                    ariaLabel='No'
+                    ariaLabel="No"
                     onClick={() => setShowUpdatePetsModal(false)}
                   >
-                    <Button.Label label='No' />
+                    <Button.Label label="No" />
                   </Button.Root>
                 </div>
               </Modal>
@@ -283,7 +244,7 @@ const Home = () => {
         </>
       )}
     </>
-  )
-}
+  );
+};
 
 export default Home;
