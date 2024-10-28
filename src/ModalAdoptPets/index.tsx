61a5030d6477c6494ed68db41a7dd2e6bea0b618
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-10-27 22:08:20 -0300

    feat: create ModalAdoptPets

---
+++ src/ModalAdoptPets/index.tsx
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import Modal from "../components/Modal";
+import Overlay from "../components/Overlay";
+import { Context } from "../context";
+import { Button } from "../components/Button";
+import { useNavigate } from "react-router-dom";
+
+const ModalAdoptPets = () => {
+  const { setShowAdoptionModal, selectedPet } = useContext(Context);
+  const navigate = useNavigate();
+  return (
+    <>
+      <Overlay openModal={setShowAdoptionModal} />
+      <Modal
+        title={`Would you like to adopt ${selectedPet[0].name}?`}
+        text="You will be redirected to the adoption form"
+      >
+        <img
+          alt={`Random picture of a ${selectedPet[0].species}`}
+          src={selectedPet[0].url}
+        />
+        <div className="modal__buttons-container">
+          <Button.Root ariaLabel="Yes" onClick={() => navigate("/adopt")}>
+            <Button.Label label="Yes" />
+          </Button.Root>
+          <Button.Root
+            ariaLabel="No"
+            onClick={() => setShowAdoptionModal(false)}
+          >
+            <Button.Label label="No" />
+          </Button.Root>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalAdoptPets;
