40a353c2a055794040dc7a4fe2bbc549e726bfc4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-01 21:02:04 -0300

    feat: add pet info to form page

--- src/pages/AdoptionForm/index.tsx
+++ src/pages/AdoptionForm/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, useEffect, useState } from "react";
+import { FormEvent, useContext, useEffect, useRef, useState } from "react";
 import { Button } from "../../components/Button";
 import Modal from "../../components/Modal";
 import Overlay from "../../components/Overlay";
@@ -8,7 +8,10 @@ import { Context } from "../../context";
 const AdoptionForm = () => {
   const { selectedPet } = useContext(Context);
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showPageNotFoundScreen, setShowPageNotFoundScreen] =
+    useState<boolean>(false);
   const navigate = useNavigate();
+  const nameRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -25,7 +28,11 @@ const AdoptionForm = () => {
   }, [showModal]);
 
   useEffect(() => {
-    document.title = `Form | Adopt ${selectedPet[0].name}`;
+    if (selectedPet.length === 0) {
+      setShowPageNotFoundScreen(true);
+    }
+    document.title = `Form | Adopt ${selectedPet[0]?.name}`;
+    nameRef.current && nameRef.current.focus();
   }, []);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -35,34 +42,42 @@ const AdoptionForm = () => {
 
   return (
     <>
-      <form className="form" onSubmit={handleSubmit}>
-        <p>{`You are filling the form to adopt ${selectedPet[0].name}.`}</p>
-        <img
-          src={selectedPet[0].url}
-          alt={`${selectedPet[0].name}'s picture`}
-        />
-        <label htmlFor="input__first-name">
-          First name:
-          <input required type="text" id="input__first-name" autoFocus />
-        </label>
-        <label htmlFor="input__last-name">
-          Last name:
-          <input required type="text" id="input__last-name" />
-        </label>
-        <label htmlFor="input__email">
-          Email:
-          <input required type="email" id="input__email" />
-        </label>
-        <label htmlFor="input__phone-number">
-          Phone number:
-          <input required type="tel" id="input__phone-number" />
-        </label>
-        <label htmlFor="input__birth-date">
-          Birth date:
-          <input required type="date" id="input__birth-date" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      {showPageNotFoundScreen ? (
+        <article>
+          <h2>Oops...</h2>
+          <p>Page not found!</p>
+        </article>
+      ) : (
+        <form className="form" onSubmit={handleSubmit}>
+          <p>{`You are filling the form to adopt ${selectedPet[0]?.name}.`}</p>
+          <img
+            src={selectedPet[0]?.url}
+            alt={`${selectedPet[0]?.name}'s picture`}
+          />
+          <hr style={{ width: "50dvw" }} />
+          <label htmlFor="input__first-name">
+            First name:
+            <input required type="text" id="input__first-name" ref={nameRef} />
+          </label>
+          <label htmlFor="input__last-name">
+            Last name:
+            <input required type="text" id="input__last-name" />
+          </label>
+          <label htmlFor="input__email">
+            Email:
+            <input required type="email" id="input__email" />
+          </label>
+          <label htmlFor="input__phone-number">
+            Phone number:
+            <input required type="tel" id="input__phone-number" />
+          </label>
+          <label htmlFor="input__birth-date">
+            Birth date:
+            <input required type="date" id="input__birth-date" />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      )}
       {showModal && (
         <>
           <Overlay openModal={setShowModal} />
