30369ccea68278b2f0137ae1ab13cb0c39920bc9
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-16 09:40:55 -0300

    build: setup "@" as /src path

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": "src",
+      "@": path.resolve(__dirname, "src"),
     },
   },
   test: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
     globals: true,
     environment: "jsdom",
     setupFiles: "./setupTests.ts",
