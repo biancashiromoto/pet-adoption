9a86ffe295be8399ddfd9930c377469fb07f5c28
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-17 11:27:56 -0300

    chore: rename Filter component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,17 +1,19 @@
-import { forwardRef } from 'react';
-import { FilterProps } from './index.types';
+import { forwardRef } from "react";
+import { FilterProps } from "./index.types";
 
-const Filter = forwardRef<HTMLSelectElement, FilterProps>(({ id, label, items, onChange }, ref) => (
-  <div className='filter-container__filter'>
-    <label htmlFor={id}>{label}</label>
-    <select id={id} ref={ref} onChange={onChange}>
-      {items.map((item) => (
-        <option key={item} value={item}>
-          {item}
-        </option>
-      ))}
-    </select>
-  </div>
-));
+const FilterSelect = forwardRef<HTMLSelectElement, FilterProps>(
+  ({ id, label, items, onChange }, ref) => (
+    <div className="filter-container__filter">
+      <label htmlFor={id}>{label}</label>
+      <select id={id} ref={ref} onChange={onChange}>
+        {items.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+);
 
-export default Filter;
+export default FilterSelect;
