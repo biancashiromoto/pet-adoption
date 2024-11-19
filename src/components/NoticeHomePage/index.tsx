d422a291f49d3e673d3823afa6df25cedc9529f8
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-18 21:02:28 -0300

    feat: implement accessibility icon in link

--- src/components/NoticeHomePage/index.tsx
+++ src/components/NoticeHomePage/index.tsx
@@ -2,19 +2,24 @@ import { useContext } from "react";
 import Notice from "../Notice";
 import { Context } from "@/context";
 import { TiWarningOutline } from "react-icons/ti";
+import { IoOpenOutline } from "react-icons/io5";
 
 const NoticeHomePage = () => {
   const { dontShowHomePageNoticeAgain, setDontShowHomePageNoticeAgain } =
     useContext(Context);
   return (
-    <Notice>
+    <Notice className="notice__home-page">
       <p>
         <TiWarningOutline />
         This website is a demo created for learning and development purposes. No
         real adoptions take place here. For more details, see the{" "}
-        <a href="https://github.com/biancashiromoto/pet-adoption/blob/main/README.md">
-          README
-        </a>{" "}
+        <a
+          rel="noopener"
+          target="_blank"
+          href="https://github.com/biancashiromoto/pet-adoption/blob/main/README.md"
+        >
+          README <IoOpenOutline aria-label="Opens in a new tab" />
+        </a>
         file.
       </p>
       <label htmlFor="dont-show-again" className="show-notice-again">
