44ca1322856a10d7595e09e2927fc9c8aa695a34
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-20 20:54:55 -0300

    tests: update tests for Header component

--- src/components/Header/spec/index.test.tsx
+++ src/components/Header/spec/index.test.tsx
@@ -1,15 +1,64 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Header from "..";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { Context } from "@/context";
+import { ContextProps } from "@/context/index.types";
+
+vi.mock("react-router-dom", async () => {
+  const actual =
+    await vi.importActual<typeof import("react-router-dom")>(
+      "react-router-dom"
+    );
+  return {
+    ...actual,
+    useLocation: vi.fn(),
+  };
+});
 
 describe("Header component", () => {
-  it("should be correctly rendered", () => {
+  const setShowUpdatePetsModalMock = vi.fn();
+
+  const renderComponent = (pathname: string = "/") => {
+    (useLocation as any).mockReturnValue({ pathname });
     render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
+      <Context.Provider
+        value={
+          {
+            setShowUpdatePetsModal: setShowUpdatePetsModalMock,
+          } as unknown as ContextProps
+        }
+      >
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </Context.Provider>
     );
+  };
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should be correctly rendered", () => {
+    renderComponent();
     expect(screen.getByText("Pet adoption")).toBeInTheDocument();
+    expect(screen.getByTestId("logo")).toBeInTheDocument();
+    expect(screen.getByTestId("logo").getAttribute("height")).toEqual("36");
+    expect(screen.getByTestId("logo").getAttribute("width")).toEqual("36");
+    expect(screen.queryByTestId("link__return")).not.toBeInTheDocument();
+  });
+
+  it("should call setShowUpdatePetsModal when button is clicked", () => {
+    renderComponent();
+    fireEvent.click(screen.getByRole("button"));
+    expect(setShowUpdatePetsModalMock).toHaveBeenCalledWith(true);
+  });
+
+  it("should render two buttons to return to Home page if pathname is not '/'", () => {
+    renderComponent("/test");
+
+    const returnButtons = screen.getAllByLabelText(/return to home page/i);
+    expect(returnButtons.length).toEqual(2);
   });
 });
