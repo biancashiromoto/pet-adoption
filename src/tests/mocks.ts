32726c2867a6e83d5b87790b9894b1254b21d67d
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-20 20:36:53 -0300

    feat: create pets mock

---
+++ src/tests/mocks.ts
@@ -0,0 +1,24 @@
+import { Pet } from "@/types/Pet.type";
+
+export const petsMock: Pet[] = [
+  {
+    id: "_VcB1rc9l",
+    url: "image.jpg",
+    width: 10,
+    height: 10,
+    name: "Cleo",
+    age: 5,
+    species: "dog",
+    isFavorite: false,
+  },
+  {
+    id: "_VcB1rc92",
+    url: "image.jpg",
+    width: 10,
+    height: 10,
+    name: "Pancakes",
+    age: 12,
+    species: "cat",
+    isFavorite: true,
+  },
+];
