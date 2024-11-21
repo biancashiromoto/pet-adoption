453ddf9dadea7adc173031c3909235ffac5c8767
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-21 18:51:55 -0300

    feat: create Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Developed by Bianca Shiromoto</p>
+      <section>
+        <a
+          aria-label="LinkedIn"
+          href="https://www.linkedin.com/in/bshiromoto/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          aria-label="GitHub"
+          href="https://www.linkedin.com/in/bshiromoto/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
