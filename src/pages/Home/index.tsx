728bb221ef65fa409af714379d34915310c4b2b7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-21 20:35:32 -0300

    feat: implement swipe effect to toggle species

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import FiltersContainer from "@/components/FiltersContainer";
 import Loader from "@/components/Loader";
 import { Context } from "@/context";
@@ -21,8 +21,20 @@ const Home = () => {
     setShowAdoptionModal,
     showUpdatePetsModal,
     setShowUpdatePetsModal,
+    orderRef,
+    favoriteRef,
+    setSpeciesFilter,
   } = useContext(Context);
   const { isLoadingOrFetching, error, refetch } = useFetchPets();
+  useEscapeKeyClose();
+
+  const touchStartX = useRef(0);
+  const touchEndX = useRef(0);
+
+  useEffect(() => {
+    setShowUpdatePetsModal(false);
+    setShowAdoptionModal(false);
+  }, []);
 
   useUpdatePageTitle(
     showAdoptionModal && selectedPet.length > 0
@@ -37,6 +49,26 @@ const Home = () => {
     setShowAdoptionModal(false);
   }, []);
 
+  const handleTouchStart = (e: React.TouchEvent) => {
+    touchStartX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    touchEndX.current = e.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    const swipeDistance = touchStartX.current - touchEndX.current;
+
+    if (Math.abs(swipeDistance) > 50) {
+      if (swipeDistance > 0) {
+        setSpeciesFilter("dog");
+      } else {
+        setSpeciesFilter("cat");
+      }
+    }
+  };
+
   useEffect(() => {
     utils.setLocalStorage("pets", pets);
   }, [pets]);
@@ -45,7 +77,11 @@ const Home = () => {
     <div className="home">
       {error && <p>Error: {error.message}</p>}
       <FiltersContainer />
-      <main>
+      <main
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
         {isLoadingOrFetching && <Loader />}
         {!error && !isLoadingOrFetching && (
           <>
