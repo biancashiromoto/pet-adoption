fa239e96fa19ac3dddfd13dad4f9dd479c621f30
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-21 18:51:46 -0300

    refactor: render no-pets when isLoadingOrFetching is false

--- src/components/PetList/index.tsx
+++ src/components/PetList/index.tsx
@@ -3,17 +3,19 @@ import { useContext } from "react";
 import Card from "../Card";
 import { Pet } from "@/types/Pet.type";
 import useToggleFavorite from "@/hooks/useToggleFavorite";
+import useFetchPets from "@/hooks/useFetchPets";
 
 const PetList = () => {
   const { displayedPets, setSelectedPet, setShowAdoptionModal } =
     useContext(Context);
   const { toggleFavorite } = useToggleFavorite();
+  const { isLoadingOrFetching, data: pets } = useFetchPets();
 
   return (
     <div
       className={`pet-list ${displayedPets.length === 0 || !displayedPets ? "offwhite" : ""}`}
     >
-      {(displayedPets.length === 0 || !displayedPets) && (
+      {(!pets || pets.length === 0) && !isLoadingOrFetching && (
         <h2 className="no-pets">No pets found :/</h2>
       )}
       {displayedPets.length > 0 &&
