8c56149dd3460f8e21613ea73ea624ad44d04251
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-20 22:21:10 -0300

    refactor: update Modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,13 +1,40 @@
-import { ModalProps } from "./index.types";
+import { FC, ReactNode } from "react";
 
-const Modal = ({ children, className, title, text, ...rest }: ModalProps) => {
+type Props = {
+  children: ReactNode;
+  className?: string;
+  isVisible: boolean;
+  onClose: () => void;
+  title?: string;
+  text?: string;
+  wrapperClassName: string;
+};
+
+const Modal: FC<Props> = ({
+  children,
+  className,
+  isVisible = true,
+  onClose,
+  title,
+  text,
+  ...rest
+}) => {
+  if (!isVisible) return null;
+
   return (
-    <div className="modal" {...rest}>
-      <section>
+    <div className={`modal ${className}`} {...rest}>
+      <section className="modal-body">
         <h2>{title}</h2>
-        <p>{text}</p>
+
+        {text && <p>{text}</p>}
+        {children}
       </section>
-      {children}
+
+      {onClose && (
+        <button type="button" className="modal-close-button" onClick={onClose}>
+          Close
+        </button>
+      )}
     </div>
   );
 };
