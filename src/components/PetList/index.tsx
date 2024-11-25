a98ad051713c7a94f68014a283a63f26f8752c9c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-11-25 18:27:42 -0300

    refactor: condition "No pets found" message

--- src/components/PetList/index.tsx
+++ src/components/PetList/index.tsx
@@ -3,19 +3,20 @@ import { useContext } from "react";
 import Card from "../Card";
 import { Pet } from "@/types/Pet.type";
 import useToggleFavorite from "@/hooks/useToggleFavorite";
+import useFetchPets from "@/hooks/useFetchPets";
 
 const PetList = () => {
   const { displayedPets, setSelectedPet, setShowAdoptionModal } =
     useContext(Context);
   const { toggleFavorite } = useToggleFavorite();
+  const { isLoadingOrFetching } = useFetchPets();
 
   return (
     <div
       className={`pet-list ${displayedPets.length === 0 || !displayedPets ? "offwhite" : ""}`}
     >
-      {(displayedPets.length === 0 || !displayedPets) && (
-        <h2 className="no-pets">No pets found :/</h2>
-      )}
+      {(displayedPets.length === 0 || !displayedPets) &&
+        !isLoadingOrFetching && <h2 className="no-pets">No pets found :/</h2>}
       {displayedPets.length > 0 &&
         displayedPets.map((pet: Pet) => (
           <Card
