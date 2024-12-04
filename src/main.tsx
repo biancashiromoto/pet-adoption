1bb03c15953a1ec7dbcf7bfeb89d3d85426d3621
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-12-04 20:39:40 -0300

    build: install react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./styles/index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Provider from "./context/Provider";
 import App from "./App";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")!).render(
       <Provider>
         <App />
       </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
 );
