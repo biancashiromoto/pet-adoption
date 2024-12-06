1c42a30456dd112ec4d07290410b5f2ff1ba29eb
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-12-06 20:23:09 -0300

    chore: update info

--- src/helpers/info.ts
+++ src/helpers/info.ts
@@ -1,8 +1,4 @@
-const MAX_PETS: number = 15;
-
-export const info = {
-  CAT_API_KEY: 'live_zllOBSsaSmrsLy6r2n5z2SQ7Zqz4NkckgTWwPzxZJr90rcoeMUpSleldcvpv8v9r',
-  CAT_API_URL: `https://api.thecatapi.com/v1/images/search?limit=${MAX_PETS}&size=thumb `,
-  DOG_API_URL: `https://api.thedogapi.com/v1/images/search?limit=${MAX_PETS}&size=thumb `,
-  DOG_API_KEY: 'live_fjag8bs7S11T5sFU6WhjVtUBRslLyueJ97ohSNbjCn7hERUx5mjZQMa5XndLrQME',
-}
+export const API_KEYS = {
+  cat: "live_zllOBSsaSmrsLy6r2n5z2SQ7Zqz4NkckgTWwPzxZJr90rcoeMUpSleldcvpv8v9r",
+  dog: "live_fjag8bs7S11T5sFU6WhjVtUBRslLyueJ97ohSNbjCn7hERUx5mjZQMa5XndLrQME",
+};
